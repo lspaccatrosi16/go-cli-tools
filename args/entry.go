027c3c1f5dc82de1fcba0d3c4c53e5ab45866c56
Luca Spaccatrosi <l.spaccatrosi@outlook.com>
2024-06-24 07:50:20 +0100

    arg parser

---
+++ args/entry.go
@@ -0,0 +1,150 @@
+package args
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"text/tabwriter"
+)
+
+type entry struct {
+	name        string
+	short       string
+	description string
+}
+
+func (e *entry) Name() string {
+	return e.name
+}
+
+func (e *entry) Short() string {
+	return e.short
+}
+
+func (e *entry) Description() string {
+	return e.description
+}
+
+type stringEntry struct {
+	entry
+	value string
+}
+
+func (e *stringEntry) Default() string {
+	return e.value
+}
+
+type numberEntry struct {
+	entry
+	value int
+}
+
+func (e *numberEntry) Default() string {
+	return fmt.Sprintf("%d", e.value)
+}
+
+type boolEntry struct {
+	entry
+	value bool
+}
+
+func (e *boolEntry) Default() string {
+	return fmt.Sprintf("%t", e.value)
+}
+
+func NewStringEntry(name, short, description string, defaultVal string) ListEntry {
+	return &stringEntry{
+		entry: entry{
+			name:        name,
+			short:       short,
+			description: description,
+		},
+		value: defaultVal,
+	}
+}
+
+func NewNumberEntry(name, short, description string, defaultVal int) ListEntry {
+	return &numberEntry{
+		entry: entry{
+			name:        name,
+			short:       short,
+			description: description,
+		},
+		value: defaultVal,
+	}
+}
+
+func NewBoolEntry(name, short, description string, defaultVal bool) ListEntry {
+	return &boolEntry{
+		entry: entry{
+			name:        name,
+			short:       short,
+			description: description,
+		},
+		value: defaultVal,
+	}
+}
+
+type ListEntry interface {
+	Name() string
+	Short() string
+	Description() string
+	Default() string
+}
+
+var entries = map[string]ListEntry{
+	"h": NewBoolEntry("help", "h", "show help", false),
+	"v": NewBoolEntry("version", "v", "show version", false),
+}
+
+func RegisterEntry(e ListEntry) {
+	entries[e.Name()] = e
+}
+
+func usage() string {
+	buf := bytes.NewBuffer(nil)
+	tr := tabwriter.NewWriter(buf, 0, 0, 3, ' ', 0)
+	for _, v := range entries {
+		fmt.Fprintf(tr, "%s\t-%s\t%s\t(%s)\n", v.Name(), v.Short(), v.Description(), v.Default())
+	}
+	tr.Flush()
+	return buf.String()
+}
+
+var transformed map[string]any
+
+func transformEntries() {
+	ents := map[string]any{}
+	for _, v := range entries {
+		switch v := v.(type) {
+		case *stringEntry:
+			fv := flag.String(v.short, v.value, v.description)
+			ents[v.name] = fv
+		case *numberEntry:
+			fv := flag.Int(v.short, v.value, v.description)
+			ents[v.name] = fv
+		case *boolEntry:
+			fv := flag.Bool(v.short, v.value, v.description)
+			ents[v.name] = fv
+		}
+	}
+	transformed = ents
+}
+
+func GetFlagValue[T any](name string) (T, error) {
+	var blank T
+	if transformed == nil {
+		return blank, wrap(fmt.Errorf("get flag value called before flag parse"))
+	}
+
+	v, ok := transformed[name]
+	if !ok {
+		return blank, wrap(fmt.Errorf("flag %s not found", name))
+	}
+
+	if fv, ok := v.(*T); ok {
+		return *fv, nil
+	}
+
+	return blank, fmt.Errorf("flag %s is not of type %T", name, blank)
+}
---
+++ args/opts.go
@@ -0,0 +1,58 @@
+package args
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/lspaccatrosi16/go-cli-tools/internal/pkgError"
+)
+
+var parsed bool
+
+var args []string
+var version string
+
+var wrap = pkgError.WrapErrorFactory("args")
+
+var writer = os.Stdout
+
+func SetVersion(v string) {
+	version = v
+}
+
+var SetWriter = func(w *os.File) {
+	writer = w
+}
+
+func ParseOpts() error {
+	if !flag.Parsed() {
+		transformEntries()
+		flag.Parse()
+		rem := flag.Args()
+		if h, err := GetFlagValue[bool]("help"); err == nil && h {
+			fmt.Fprintln(writer, usage())
+			os.Exit(0)
+		} else if err != nil {
+			return err
+		}
+
+		if v, err := GetFlagValue[bool]("version"); err == nil && v {
+			fmt.Fprintln(writer, version)
+			os.Exit(0)
+		} else if err != nil {
+			return err
+		}
+
+		args = rem
+		parsed = true
+	}
+	return nil
+}
+
+func GetArgs() []string {
+	if !parsed {
+		return nil
+	}
+	return args
+}
